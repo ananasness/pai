a83d8e96a239a704cdb93a64f943ef144639d272
Ilya Pyatkin <baldurmods@gmail.com>
2018-01-19 14:35:47 +0300

    Added input move  and sessions handling
    Relocated server.py

--- tic-tac-toe/server.py
+++ tic-tac-toe/server.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
+from minmax_alg import MinMax
 app = Flask(__name__)
 TOKEN = "Bearer"
+sessions = {}
 
 @app.route("/")
 def index():
@@ -8,8 +10,16 @@ def index():
 
 @app.route('/tictactoe/<int:session_id>/', methods=["POST"])
 def tictactoe(session_id):
+	if session_id not in sessions:
+		sessions[session_id] = MinMax()
 	if not request.headers.get("Authorization") == TOKEN:
 		return "", 401
-	return "nope"
-	#return game(session_id, request.get_data())
 
+	try: 
+		if request.get_data():
+			move = int(request.get_data())
+		else:
+			move = -1
+	except ValueError:
+		return "", 403
+	return "", sessions[session_id].play(move)

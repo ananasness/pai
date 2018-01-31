855bb1fba7ca36cd7794905b3aa05706811d931c
Ilya Pyatkin <baldurmods@gmail.com>
2018-01-31 21:29:45 +0300

    Added the main bot functionality
    Also edited Matches to work with the bot.

--- tic-tac-toe/matches.py
+++ tic-tac-toe/matches.py
@@ -1,25 +1,24 @@
 class Matches:
-
     def __init__(self):
         self.state = 21
 
     def play(self, state):
-        if not self.state_valid(state):
-            return 0, 403
+        # Bot loose condition
+        if state == 1:
+            return 1
+        # Bot win condition
+        if state == 0:
+            return 2
 
-        if state in [0, 13]:
-            return 0, 418
-
-        lose_states = [1 + 4*x for x in range(6) if 1 + 4*x <= state]
+        lose_states = [1 + 4 * x for x in range(6) if 1 + 4 * x <= state]
 
         if state in lose_states:
             self.state -= 1
-
         else:
             self.state = lose_states[-1]
 
-        return self.state, 200
+        return 0
 
     def state_valid(self, state):
-        return self.state - state in [1, 2, 3] \
+        return 1 <= self.state - state <= 3 \
                and 0 <= state <= 21

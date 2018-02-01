d9826b1e8df0344f52a35526b38f0404ef932926
bulnab <bulnab@gmail.com>
2018-02-01 03:14:00 +0300

    Correct winner checking in Gomoku

--- tic-tac-toe/gomoku.py
+++ tic-tac-toe/gomoku.py
@@ -12,22 +12,18 @@ class Gomoku:
 
     def play(self, x: int, y: int):
         if not self.is_move_valid(x, y):
-            check_result = self.check()
-            if check_result == 1 or check_result == 2:
-                return self.state, check_result
-
-            n_empty_cells = 0
-            for i in range(self.n_cells):
-                for j in range(self.n_cells):
-                    if self.state[i][j] == self.empty_piece:
-                        n_empty_cells += 1
-
-            if n_empty_cells == 0:
-                return self.state, 3
+            # status = self.check_all()
+            # if status != 0:
+            #     return self.state, status
 
             return self.state, 4
         else:
             self.state[y][x] = self.player_1_piece
+
+            status = self.check_all()
+            if status != 0:
+                return self.state, status
+
             while True:
                 bx = randint(0, 7)
                 by = randint(0, 7)
@@ -43,18 +39,9 @@ class Gomoku:
             4 - invalid move
             """
 
-            check_result = self.check()
-            if check_result == 1 or check_result == 2:
-                return self.state, check_result
-
-            n_empty_cells = 0
-            for i in range(self.n_cells):
-                for j in range(self.n_cells):
-                    if self.state[i][j] == self.empty_piece:
-                        n_empty_cells += 1
-
-            if n_empty_cells == 0:
-                return self.state, 3
+            status = self.check_all()
+            if status != 0:
+                return self.state, status
 
             return self.state, 0
 
@@ -66,126 +53,85 @@ class Gomoku:
         else:
             return False
 
-    def has_record(self, y, x):
-        return not self.state[y][x] == self.empty_piece
-
-    def check_row(self, y, x):
-        if self.has_record(x, y) and \
-                self.has_record(x, y + 1) and \
-                self.has_record(x, y + 2) and \
-                self.has_record(x, y + 3) and \
-                self.has_record(x, y + 4):
-
-            if self.state[x][y] == self.player_1_piece and \
-                    self.state[x][y + 1] == self.player_1_piece and \
-                    self.state[x][y + 2] == self.player_1_piece and \
-                    self.state[x][y + 3] == self.player_1_piece and \
-                    self.state[x][y + 4] == self.player_1_piece:
-                return 1
-
-            elif self.state[x][y] == self.player_2_piece and \
-                    self.state[x][y + 1] == self.player_2_piece and \
-                    self.state[x][y + 2] == self.player_2_piece and \
-                    self.state[x][y + 3] == self.player_2_piece and \
-                    self.state[x][y + 4] == self.player_2_piece:
-                return 2
+    def check_row(self, x, y, piece):
+        if self.state[y][x] == piece and \
+                self.state[y][x + 1] == piece and \
+                self.state[y][x + 2] == piece and \
+                self.state[y][x + 3] == piece and \
+                self.state[y][x + 4] == piece:
+            return True
         else:
-            return 0
+            return False
 
-    def check_col(self, y, x):
-        if self.has_record(x, y) and \
-                self.has_record(x + 1, y) and \
-                self.has_record(x + 2, y) and \
-                self.has_record(x + 3, y) and \
-                self.has_record(x + 4, y):
-
-            if self.state[x][y] == self.player_1_piece and \
-                    self.state[x + 1][y] == self.player_1_piece and \
-                    self.state[x + 2][y] == self.player_1_piece and \
-                    self.state[x + 3][y] == self.player_1_piece and \
-                    self.state[x + 4][y] == self.player_1_piece:
-                return 1
-
-            elif self.state[x][y] == self.player_2_piece and \
-                    self.state[x + 1][y] == self.player_2_piece and \
-                    self.state[x + 2][y] == self.player_2_piece and \
-                    self.state[x + 3][y] == self.player_2_piece and \
-                    self.state[x + 4][y] == self.player_2_piece:
-                return 2
+    def check_col(self, x, y, piece):
+        if self.state[y][x] == piece and \
+                self.state[y + 1][x] == piece and \
+                self.state[y + 2][x] == piece and \
+                self.state[y + 3][x] == piece and \
+                self.state[y + 4][x] == piece:
+            return True
         else:
-            return 0
+            return False
 
-    def check_up(self, y, x):
-        if self.has_record(x, y) and \
-                self.has_record(x + 1, y + 1) and \
-                self.has_record(x + 2, y + 2) and \
-                self.has_record(x + 3, y + 3) and \
-                self.has_record(x + 4, y + 4):
-
-            if self.state[x][y] == self.player_1_piece and \
-                    self.state[x + 1][y + 1] == self.player_1_piece and \
-                    self.state[x + 2][y + 2] == self.player_1_piece and \
-                    self.state[x + 3][y + 3] == self.player_1_piece and \
-                    self.state[x + 4][y + 4] == self.player_1_piece:
-                return 1
-
-            elif self.state[x][y] == self.player_2_piece and \
-                    self.state[x + 1][y + 1] == self.player_2_piece and \
-                    self.state[x + 2][y + 2] == self.player_2_piece and \
-                    self.state[x + 3][y + 3] == self.player_2_piece and \
-                    self.state[x + 4][y + 4] == self.player_2_piece:
-                return 2
+    def check_main_diag(self, x, y, piece):
+        if self.state[y][x] == piece and \
+                self.state[y + 1][x + 1] == piece and \
+                self.state[y + 2][x + 2] == piece and \
+                self.state[y + 3][x + 3] == piece and \
+                self.state[y + 4][x + 4] == piece:
+            return True
         else:
-            return 0
+            return False
 
-    def check_down(self, y, x):
-        if self.has_record(x, y) and \
-                self.has_record(x + 1, y - 1) and \
-                self.has_record(x + 2, y - 2) and \
-                self.has_record(x + 3, y - 3) and \
-                self.has_record(x + 4, y - 4):
-
-            if self.state[x][y] == self.player_1_piece and \
-                    self.state[x + 1][y - 1] == self.player_1_piece and \
-                    self.state[x + 2][y - 2] == self.player_1_piece and \
-                    self.state[x + 3][y - 3] == self.player_1_piece and \
-                    self.state[x + 4][y - 4] == self.player_1_piece:
-                return 1
-
-            elif self.state[x][y] == self.player_2_piece and \
-                    self.state[x + 1][y - 1] == self.player_2_piece and \
-                    self.state[x + 2][y - 2] == self.player_2_piece and \
-                    self.state[x + 3][y - 3] == self.player_2_piece and \
-                    self.state[x + 4][y - 4] == self.player_2_piece:
-                return 2
-
+    def check_anti_diag(self, x, y, piece):
+        if self.state[y][x] == piece and \
+                self.state[y + 1][x - 1] == piece and \
+                self.state[y + 2][x - 2] == piece and \
+                self.state[y + 3][x - 3] == piece and \
+                self.state[y + 4][x - 4] == piece:
+            return True
         else:
-            return 0
+            return False
 
-    def check(self):
-        for i in range(self.n_cells):
-            for j in range(self.n_cells - 4):
-                result = self.check_row(j, i)
-                if result != 0:
-                    return result
+    def check_all(self):
+        for x in range(self.n_cells - 4):
+            for y in range(self.n_cells):
+                if self.check_row(x, y, self.player_1_piece):
+                    return 1
+                if self.check_row(x, y, self.player_2_piece):
+                    return 2
 
-        for i in range(self.n_cells - 4):
-            for j in range(self.n_cells):
-                result = self.check_col(j, i)
-                if result != 0:
-                    return result
+        for x in range(self.n_cells):
+            for y in range(self.n_cells - 4):
+                if self.check_col(x, y, self.player_1_piece):
+                    return 1
+                if self.check_col(x, y, self.player_2_piece):
+                    return 2
 
-        for i in range(self.n_cells - 4):
-            for j in range(self.n_cells - 4):
-                result = self.check_up(j, i)
-                if result != 0:
-                    return result
+        for x in range(self.n_cells - 4):
+            for y in range(self.n_cells - 4):
+                if self.check_main_diag(x, y, self.player_1_piece):
+                    return 1
+                if self.check_main_diag(x, y, self.player_2_piece):
+                    return 2
 
-        for i in range(self.n_cells - 4):
-            for j in range(4, self.n_cells):
-                result = self.check_down(j, i)
-                if result != 0:
-                    return result
+        for x in range(4, self.n_cells):
+            for y in range(self.n_cells - 4):
+                if self.check_anti_diag(x, y, self.player_1_piece):
+                    return 1
+                if self.check_anti_diag(x, y, self.player_2_piece):
+                    return 2
+
+        n_empty_cells = 0
+        for x in range(self.n_cells):
+            for y in range(self.n_cells):
+                if self.state[y][x] == self.empty_piece:
+                    n_empty_cells += 1
+
+        if n_empty_cells == 0:
+            return 3
+
+        return 0
 
     def __str__(self):
         return str(self.state).replace('], [', '],\n [')
@@ -200,7 +146,7 @@ if __name__ == '__main__':
     print(game, status)
     state, status = game.play(0, 2)
     print(game, status)
-    state, status = game.play(0, 3)
+    state, status = game.play(0, 0)
     print(game, status)
     state, status = game.play(0, 4)
     print(game, status)
@@ -221,7 +167,7 @@ if __name__ == '__main__':
     print(game, status)
     state, status = game.play(1, 4)
     print(game, status)
-    state, status = game.play(1, 5)
+    state, status = game.play(1, 0)
     print(game, status)
     state, status = game.play(1, 6)
     print(game, status)
@@ -232,7 +178,7 @@ if __name__ == '__main__':
     print(game, status)
     state, status = game.play(2, 1)
     print(game, status)
-    state, status = game.play(2, 2)
+    state, status = game.play(2, 0)
     print(game, status)
     state, status = game.play(2, 3)
     print(game, status)
